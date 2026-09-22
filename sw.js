6eff4d6acbc4b8c7bdb10258f04b3c6d25f58663
Batuhan Tuğberk İLGAR <batuhan.ilgar@8bitiz.com>
2026-09-22 22:33:58 +0300

    Publish autumn wedding planning and attendance confirmation guides

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker for Zeynep & Batuhan Wedding Invitation
 // Enables offline functionality
 
-const CACHE_NAME = 'zeynep-batuhan-v34';
+const CACHE_NAME = 'zeynep-batuhan-v35';
 // Cevrimdisi sayfa kabugu. Medya ve gorseller tarayicinin ag akisina birakilir.
 const urlsToCache = [
   '/',
